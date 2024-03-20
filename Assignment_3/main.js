c88ae90f887684cf50caba918ffa2c29313f2021
“[Minhaj <[mminhajsiddiqui@gmail.com]>
2024-03-20 22:28:07 +0500

    second commit

---
+++ Assignment_3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Storing the Famous Qoute and its Author
+let quote = "A person who never made a mistake never tried anything new.";
+let author = "Albert Einstein once said,";
+// Printing the Message
+console.log(`$"{quote}" - ${author}`);
---
+++ Assignment_3/main.ts
@@ -0,0 +1,9 @@
+// Storing the Famous Qoute and its Author
+
+let quote:string = "A person who never made a mistake never tried anything new.";
+
+let author:string = "Albert Einstein once said,";
+
+// Printing the Message
+
+console.log(`$"{quote}" - ${author}`);
